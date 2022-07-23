9b5f302b6a953911d04908688d6eb13e81e4ccf4
Imran11 <Annex@110211@>
2022-07-23 18:35:56 +0600

    git init

---
+++ src/main/kotlin/Variables.kt
@@ -0,0 +1,14 @@
+fun main(arg: Array<String>) {
+
+    var score: Int = 256
+    var wicket: Int = 10
+    var over: Float= 49.5F
+    var teamName: String= "Australia"
+
+
+    println(score)
+    println(wicket)
+    println(over)
+    println(teamName)
+}
+
